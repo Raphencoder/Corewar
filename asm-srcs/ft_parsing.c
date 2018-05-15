68fac990b4167abd988ac913be6b96e91f3a0196
Raphael KRIEF <rkrief@e1r6p16.42.fr>
2018-05-15 20:16:09 +0200

    finish parsing

--- asm-srcs/ft_parsing.c
+++ asm-srcs/ft_parsing.c
@@ -6,15 +6,64 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/14 16:05:57 by rkrief            #+#    #+#             */
-/*   Updated: 2018/05/14 19:42:18 by rkrief           ###   ########.fr       */
+/*   Updated: 2018/05/15 20:15:35 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../lem-in/libft/libft.h"
+#include "../includes/op.h"
 #include <stdio.h>
 
-#define NAME_CMD_STRING			".name"
-#define COMMENT_CMD_STRING		".comment"
+void    ft_is_an_error(char *str, int i);
+
+int		ft_complete_name(char *str, int i, header_t *header)
+{
+	int j;
+
+	j = 0;
+	while (str[i] <= 32)
+		i++;
+	if (str[i] != 34)
+		return (-1);
+	i++;
+	while (str[i])
+	{
+		if (str[i] == 34)
+			break ;
+		header->prog_name[j] = str[i];
+		j++;
+		i++;
+	}
+	if (str[i] != 34)
+		return (-1);
+	header->prog_name[j] = '\0';
+	return (i);
+}
+
+
+int		ft_complete_comment(char *str, int i, header_t *header)
+{
+	int j;
+
+	j = 0;
+	while (str[i] <= 32)
+		i++;
+	if (str[i] != 34)
+		return (-1);
+	i++;
+	while (str[i])
+	{
+		if (str[i] == 34)
+			break ;
+		header->comment[j] = str[i];
+		j++;
+		i++;
+	}
+	if (str[i] != 34)
+		return (-1);
+	header->comment[j] = '\0';
+	return (i);
+}
 
 void	ft_pass_comment(char *str, int *i)
 {
@@ -32,27 +81,71 @@ void	ft_pass_comment(char *str, int *i)
 	}
 }
 
-void	ft_parsing(char *str)
+void	ft_get_comment_then_name(header_t *header, char *str, int i)
+{
+	char	*tmp;
+	int		j;
+
+	j = ft_complete_comment(str, i + 8, header);
+	if (j == -1)
+		ft_is_an_error(str, i);
+	i = j + 1;
+	if (str[i] != '.' && str[i] != '#' && str[i] > 32)
+		ft_is_an_error(str, i);
+	ft_pass_comment(str, &i);
+	tmp = ft_strnmdup(str, i, i + 5);
+	if (!ft_strequ(NAME_CMD_STRING, tmp))
+		ft_is_an_error(str, i + );
+	if (!ft_complete_name(str, i + 5, header))
+		ft_is_an_error(str, i);
+}
+
+void	ft_get_name_then_comment(header_t *header, char *str, int i)
+{
+	char	*tmp;
+	int		j;
+
+	j = ft_complete_name(str, i + 5, header);
+	if (j == -1)
+		ft_is_an_error(str, i);
+	i = j + 1;
+	if (str[i] != '.' && str[i] != '#' && str[i] > 32)
+		ft_is_an_error(str, i);
+	ft_pass_comment(str, &i);
+	tmp = ft_strnmdup(str, i, i + 8);
+	if (!ft_strequ(COMMENT_CMD_STRING, tmp))
+		ft_is_an_error(str, i);
+	if (!ft_complete_comment(str, i + 8, header))
+		ft_is_an_error(str, i);
+}
+
+void	ft_parsing(char *str, header_t *header)
 {
 	int		i;
 	char	*tmp;
+	char	*tmpp;
 
 	i = 0;
 	if (str[i] == '#')
 		ft_pass_comment(str, &i);
 	tmp = ft_strnmdup(str, i, i + 5);
-	if (!ft_strequ(NAME_CMD_STRING, tmp))
+	tmpp = ft_strnmdup(str, i, i + 8);
+	if (!ft_strequ(NAME_CMD_STRING, tmp) && !ft_strequ(COMMENT_CMD_STRING, tmpp))
 	{
-		ft_putendl("false");
-		return ;
-	}
-	ft_complete_name(str, i + 5)
-	ft_pass_comment(str, &i);
-	ft_strdel(&tmp);
-	tmp = ft_strnmdup(str, i, i + 8);
-	if (!ft_strequ(COMMENT_CMD_STRING, tmp))
-	{
-		ft_putendl("comment false");
-		return ;
-	}
+		if (ft_strequ(NAME_CMD_STRING,tmp))
+			ft_is_an_error(str, ft_strlen(tmpp));
+		ft_is_an_error(str, ft_strlen(tmp));
+	}	
+	if (ft_strequ(NAME_CMD_STRING, tmp))
+		ft_get_name_then_comment(header, str, i);
+	else if (ft_strequ(COMMENT_CMD_STRING, tmpp))
+		ft_get_comment_then_name(header, str, i);
+	else
+		ft_is_an_error(str, i);
+	ft_putstr("le name est : ");
+	ft_putendl(header->prog_name);
+	ft_putendl("fin");
+	ft_putstr("le comment est : ");
+	ft_putendl(header->comment);
+	ft_putendl("fin");
 }
--- main.c
+++ main.c
@@ -6,16 +6,38 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/14 18:35:51 by rkrief            #+#    #+#             */
-/*   Updated: 2018/05/14 18:40:39 by rkrief           ###   ########.fr       */
+/*   Updated: 2018/05/15 14:08:10 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../lem-in/libft/libft.h"
+#include <fcntl.h>
 void	ft_parsing(char *str);
 
-int main()
+int main(int argc, char **argv)
 {
-	char *str="#frjfhjkdfsfndfkv\n.name'dsfadf'\n#fsdlfdgfdds\n.comment'fdsfsdf'";
+	int		fd;
+	char	*str;
+	char	*getall;
+	char	*tmp;
 
-	ft_parsing(str);
+	getall = NULL;
+	if (!(fd = open(argv[1], O_RDONLY)))
+		return (0);
+	while (get_next_line(fd, &str))
+	{
+		if (getall == NULL)
+			getall = ft_strdup(str);
+		else
+			{
+				tmp = getall;
+				getall = ft_strjoin(tmp, "\n");
+				ft_strdel(&tmp);
+				tmp = getall;
+				getall = ft_strjoin(tmp, str);
+				ft_strdel(&tmp);	
+			}
+	}
+	ft_parsing(getall);
 	return (0);
 }
