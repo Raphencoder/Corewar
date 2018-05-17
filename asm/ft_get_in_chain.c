454235a41d38c752398b6d773d51da361040e1a5
Raphael KRIEF <rkrief@e2r12p6.42.fr>
2018-05-17 20:27:02 +0200

    the parsing is over but bug with the linked list

--- asm/ft_get_in_chain.c
+++ asm/ft_get_in_chain.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_get_in_chain.c                                 :+:      :+:    :+:   */
+/*    ft_get_in_chain.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 11:04:44 by rkrief            #+#    #+#             */
-/*   Updated: 2018/05/17 12:00:00 by rkrief           ###   ########.fr       */
+/*   Updated: 2018/05/17 20:20:19 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,210 @@
 #include "../includes/op.h"
 #include <stdio.h>
 
+
+void	ft_is_an_error(char *str, int i);
+
+void	ft_pass_comment1(char *str, int *i)
+{
+	while (str[*i])
+	{
+		while (str[*i] && str[*i] != '\n')
+			*i = *i + 1;
+		if (str[*i + 1] && (str[*i + 1] != '#' && str[*i + 1] != '\n'))
+		{
+			*i = *i + 1;
+			return ;
+		}
+		if (str[*i + 1])
+			*i = *i + 1;
+	}
+}
+
+int		ft_take_nb_argument(char *str, int i)
+{
+	int nb_separator;
+
+	nb_separator = 0;
+	while (str[i] != '\n')
+	{
+		if (str[i] == SEPARATOR_CHAR)
+			nb_separator++;
+		i++;
+	}
+	return (nb_separator + 1);
+}
+
+int		ft_is_lib(char *str, int i)
+{
+	if (str[i] != 'r')
+		return (0);
+	i++;
+	if (ft_isdigit(str[i]) && (ft_isdigit(str[i + 1]) || str[i + 1] == ' ' ||
+				str[i + 1] == '\t' || str[i + 1] == SEPARATOR_CHAR))
+		return (1);
+	return (0);
+}
+
+t_chain	*ft_complete_content(t_chain *block, char *str, int *i)
+{
+	int direct;
+	int	j;
+	t_chain *new;
+
+	new = ft_memalloc(sizeof(t_chain));
+	direct = 0;
+	j = 0;
+	while (str[*i] == ' ' || str[*i] == '\t')
+		*i = *i + 1;
+	while (str[*i] != ' ' && str[*i] != '\t' && str[*i] != '\n')
+	{
+		if (direct && (str[*i] != LABEL_CHAR || ft_atoi(str + *i)))
+			ft_is_an_error(str, *i);
+		if (str[*i] == DIRECT_CHAR)
+			direct++;
+		if (direct > 1)
+			ft_is_an_error(str, *i);
+		block->content[j++] = str[*i];
+		*i = *i + 1;
+	}
+	block->category = "INSTRUCTION";
+	ft_putendl("finish one block");
+	ft_putendl(block->content);
+	new = block->next;
+	return (new);
+}
+
+
+//int	ft_check_if_instruction(t_chain *block)
+/*{
+	int i;
+
+	i = 0;
+	while (i < 16)
+	{
+		if (ft_strequ(op_tab[i][0], block->content))
+		{
+			block->nb_op_tab = i;
+			return (1);
+		}
+		i++;	
+	}
+	return (0);
+}
+*/
+t_chain		*ft_put_line_in_block(t_chain *block, int *i, char *str)
+{
+	int	nb_arg;
+	int j;
+	t_chain *new;
+
+	while (str[*i] == ' ' || str[*i] == '\t')
+		*i = *i + 1;
+	j = *i;
+	while (str[*i] >= 'a' && str[*i] <= 'z')
+	{
+		block->content[j++] = str[*i];
+		*i = *i + 1;
+	}
+//	if (!(ft_check_if_instruction(block)))
+//		ft_is_an_error(str, j);
+	while (str[*i] == ' ' || str[*i] == '\t')
+		*i = *i + 1;
+	nb_arg = ft_take_nb_argument(str, *i);
+//	if (nb_arg != op_tab[block->nb_op_tab][1])
+//	{
+//		ft_putendl("wrong number of argument");
+//		ft_is_an_error(str, *i);
+//	}
+//	if ((op_tab[block->nb_op_tab][2] == 1) && 
+//			(op_tab[block->nb_op_tab][3] == T_DIR && str[*i] != DIRECT_CHAR))
+//		ft_is_an_error(str, *i);
+//	else if ((block->nb_op_tab == 2 || block->nb_op_tab == 3 || block->nb_op_tab
+//				== 4 || block->nb_op_tab == 16 || block->nb_op_tab == 10) && ft_is_lib(str, *i))
+//		ft_is_an_error(str, *i);
+	while (nb_arg)
+	{
+		new = ft_complete_content(block, str, i);
+		*i = *i + 1;
+		nb_arg--;
+	}
+	return (new);
+}
+
+
+t_chain	*ft_put_label_in_block(t_chain *block, int *i, char *str, int j)
+{
+	int k;
+	t_chain *new;
+
+	
+	ft_putendl("test");
+	k = 0;
+	block->category = "LABEL";
+	block->content = ft_strnmdup(str, *i, j + 1);
+	ft_putendl("test");
+	new = ft_memalloc(sizeof(t_chain));
+	new = block->next;
+	return (new);
+}
+
+t_chain	*ft_put_in_block(t_chain *block, int *j, char *str)
+{
+	int i;
+	int	pos;
+	t_chain *new;
+
+	i = *j;
+	while (str[*j] != '\n')
+	{
+		if (str[*j] == LABEL_CHAR && str[*j - 1] != DIRECT_CHAR)
+		{
+			new = ft_put_label_in_block(block, &i, str, *j);
+			ft_putendl("test");
+			return (new);
+		}
+		*j = *j + 1;
+	}
+	new = ft_put_line_in_block(block, &i, str);
+	*j = i;
+	return (new);
+}
+
+void	ft_pass_space(char *str, int *j)
+{
+	int i;
+
+	i = 0;
+	while (str[*j] < 32)
+	{
+		if (str[*j] == '\n')
+			return ;
+		*j = *j + 1;
+	}
+}
+
 void	ft_get_in_chain(char *str, int j)
 {
-	ft_putstr("---");
-	ft_putchar(str[j]);
-	ft_putnbr(j);
-	ft_putstr("---");
+	t_chain		*block;
+	void		*start;
 
+	j++;
+	block = ft_memalloc(sizeof(t_chain));
+	block->next = 0;
+	start = block;
+	ft_putendl("test");
+	while (str[j])
+	{
+		if (str[j] == '\n')
+			block->nb_lines++;
+		else if (str[j] == '#')
+			ft_pass_comment1(str, &j);
+		else if (str[j] == ' ' || str[j] == '\t')
+			ft_pass_space(str, &j);
+		else if ((!ft_strchr(LABEL_CHARS, str[j])) && str[j] != DIRECT_CHAR)
+			ft_is_an_error(str, j);
+		else
+			ft_put_in_block(block, &j, str);
+		j++;
+	}
 }
--- op.h
+++ op.h
@@ -6,66 +6,82 @@
 /*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2018/05/04 14:13:29 by alecott          ###   ########.fr       */
+/*   Updated: 2018/05/17 19:36:02 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- * ** Toutes les tailles sont en octets.
- * ** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
- * */
+ ** Toutes les tailles sont en octets.
+ ** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
+ */
 
-#ifndef OP_H
-# define OP_H
+#ifndef _OP_H_
+# define _OP_H_
 
-# define IND_SIZE				2
-# define REG_SIZE				4
-# define DIR_SIZE				REG_SIZE
+#define IND_SIZE				2
+#define REG_SIZE				4
+#define DIR_SIZE				REG_SIZE
+#define REG_CODE				1
+#define DIR_CODE				2
+#define IND_CODE				3
+#define MAX_ARGS_NUMBER			4
+#define MAX_PLAYERS				4
+#define MEM_SIZE				(4*1024)
+#define IDX_MOD					(MEM_SIZE / 8)
+#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+#define COMMENT_CHAR			'#'
+#define LABEL_CHAR				':'
+#define DIRECT_CHAR				'%'
+#define SEPARATOR_CHAR			','
+#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+#define NAME_CMD_STRING			".name"
+#define COMMENT_CMD_STRING		".comment"
+#define REG_NUMBER				16
+#define CYCLE_TO_DIE			1536
+#define CYCLE_DELTA				50
+#define NBR_LIVE				21
+#define MAX_CHECKS				10
 
-# define REG_CODE				1
-# define DIR_CODE				2
-# define IND_CODE				3
+typedef char	t_arg_type;
+#define T_REG					1
+#define T_DIR					2
+#define T_IND					4
+#define T_LAB					8
+#define PROG_NAME_LENGTH		(128)
+#define COMMENT_LENGTH			(2048)
+#define COREWAR_EXEC_MAGIC		0xea83f3
 
-# define MAX_ARGS_NUMBER			4
-# define MAX_PLAYERS				4
-# define MEM_SIZE				(4*1024)
-# define IDX_MOD					(MEM_SIZE / 8)
-# define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+typedef char    args_type_t;
 
-# define COMMENT_CHAR			'#'
-# define LABEL_CHAR				':'
-# define DIRECT_CHAR				'%'
-# define SEPARATOR_CHAR			','
-
-# define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
-
-# define NAME_CMD_STRING		".name"
-# define COMMENT_CMD_STRING		".comment"
-
-# define REG_NUMBER				16
-
-# define CYCLE_TO_DIE			1536
-# define CYCLE_DELTA			50
-# define NBR_LIVE				21
-# define MAX_CHECKS				10
-
-typedef char					t_arg_type;
-
-# define T_REG					1
-# define T_DIR					2
-# define T_IND					4
-# define T_LAB					8
-
-# define PROG_NAME_LENGTH		(128)
-# define COMMENT_LENGTH			(2048)
-# define COREWAR_EXEC_MAGIC		0xea83f3
-
-typedef struct			s_header
+struct  op_s
 {
-	unsigned int	magic;
-	char			prog_name[PROG_NAME_LENGTH + 1];
-	unsigned int	prog_size;
-	char			comment[COMMENT_LENGTH + 1];
-}						t_header;
+	char         *mnemonique;
+	char         nbr_args;
+	args_type_t  type[MAX_ARGS_NUMBER];
+	char         code;
+	int          nbr_cycles;
+	char         *comment;
+};
+
+typedef struct op_s     op_t;
+
+
+typedef struct		s_chain
+{
+	unsigned int	nb_col;
+	unsigned int	nb_lines;
+	char			*category;
+	int				nb_op_tab;
+	char			*content;
+	struct s_chain	*next;
+}					t_chain;
+
+typedef struct		header_s
+{
+	unsigned int		magic;
+	char				prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int		prog_size;
+	char				comment[COMMENT_LENGTH + 1];
+}					header_t;
 
 #endif
