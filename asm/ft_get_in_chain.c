67619daa6e7f3aef85470e96013f4cc0f584ba00
Raphael KRIEF <rkrief@e3r2p21.42.fr>
2018-05-18 17:02:03 +0200

    everything work, little bug with some player, need to ad the verification

--- asm/ft_get_in_chain.c
+++ asm/ft_get_in_chain.c
@@ -6,7 +6,7 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 11:04:44 by rkrief            #+#    #+#             */
-/*   Updated: 2018/05/18 13:30:17 by rkrief           ###   ########.fr       */
+/*   Updated: 2018/05/18 17:00:26 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,24 +49,17 @@ void	ft_complete_content(t_chain *block, char *str, int *i)
 	int	direct;
 
 	direct = 0;
-	ft_putendl("IN complete_content");
 	j = 0;
 	while (str[*i] == ' ' || str[*i] == '\t')
 		*i = *i + 1;
 	j = *i;
 	while (str[*i] > 32 && str[*i] != SEPARATOR_CHAR)
 	{
-		ft_putstr("Start ");
 		if (str[*i] == DIRECT_CHAR && (str[*i + 1] != LABEL_CHAR &&
-!ft_isnumber(str + (*i + 1))))
+!ft_isnumber(str + (*i + 1)) && str[*i + 1] != '-' ))
 		{
 			if (direct)
 				ft_is_an_error(str, *i);
-			ft_putendl("aww nooon");
-			ft_putnbr(ft_isnumber(str + *i));
-			ft_putchar('T');
-			ft_putchar('\n');
-			ft_putendl(str + *i);
 			ft_is_an_error(str, *i);
 			direct++;
 		}
@@ -76,9 +69,6 @@ void	ft_complete_content(t_chain *block, char *str, int *i)
 	}
 	block->content = ft_strnmdup(str, j, *i);
 	block->category = "INSTRUCTION";
-	ft_putendl("finish one block");
-	ft_putstr("block->contetn = ");	
-	ft_putendl(block->content);
 }
 
 
@@ -99,13 +89,13 @@ void	ft_complete_content(t_chain *block, char *str, int *i)
 	return (0);
 }
 */
-t_chain		*ft_put_line_in_block(t_chain *block, int *i, char *str)
+int		ft_put_line_in_block(t_chain *block, int *i, char *str)
 {
 	int	nb_arg;
 	int j;
-	t_chain *new;
+	int clone;
 
-	ft_putendl("IN put line in block");
+	nb_arg = 0;
 	while (str[*i] == ' ' || str[*i] == '\t')
 		*i = *i + 1;
 	j = *i;
@@ -113,16 +103,11 @@ t_chain		*ft_put_line_in_block(t_chain *block, int *i, char *str)
 		*i = *i + 1;
 	block->content = ft_strnmdup(str, j, *i);
 	block->category = "INSTRUCTION";
-	ft_putstr("block->content = ");
-	ft_putendl(block->content);
 //	if (!(ft_check_if_instruction(block)))
 //		ft_is_an_error(str, j);
 	while (str[*i] == ' ' || str[*i] == '\t')
 		*i = *i + 1;
 	nb_arg = ft_take_nb_argument(str, *i);
-	ft_putstr("nb_arg = ");
-	ft_putnbr(nb_arg);
-	ft_putchar('\n');
 //	if (nb_arg != op_tab[block->nb_op_tab][1])
 //	{
 //		ft_putendl("wrong number of argument");
@@ -134,55 +119,54 @@ t_chain		*ft_put_line_in_block(t_chain *block, int *i, char *str)
 //	else if ((block->nb_op_tab == 2 || block->nb_op_tab == 3 || block->nb_op_tab
 //				== 4 || block->nb_op_tab == 16 || block->nb_op_tab == 10) && ft_is_lib(str, *i))
 //		ft_is_an_error(str, *i);
-	new = block->next;
-	new = ft_memalloc(sizeof(t_chain));
+	block->next = ft_memalloc(sizeof(t_chain));
+	block = block->next;
+	clone = nb_arg;
 	while (nb_arg)
 	{
-		ft_complete_content(new, str, i);
+		ft_complete_content(block, str, i);
 		*i = *i + 1;
 		nb_arg--;
-		new = new->next;
-		new = ft_memalloc(sizeof(t_chain));
+		if (!nb_arg)
+			break ;
+		block->next = ft_memalloc(sizeof(t_chain));
+		block = block->next;
 	}
-	return (new);
+	return (clone);
 }
 
 
-t_chain	*ft_put_label_in_block(t_chain *block, int i, char *str, int j)
+void	ft_put_label_in_block(t_chain *block, int i, char *str, int j)
 {
 	int k;
 
-	ft_putendl("IN put label in block");
 	k = 0;
 	block->category = "LABEL";
 	block->content = ft_strnmdup(str, i, j + 1);
-	ft_putstr("label = ");
-	ft_putendl(block->content);
-	return (block->next);
+	return ;
 }
 
-t_chain	*ft_put_in_block(t_chain *block, int *j, char *str)
+int		ft_put_in_block(t_chain *block, int *j, char *str)
 {
 	int i;
 	int	pos;
-	t_chain *new;
+	int nb_arg;
 
 	i = *j;
-	ft_putendl("IN put in block");
+	nb_arg = 0;
 	while (str[*j] != '\n')
 	{
 		if (str[*j] == LABEL_CHAR && str[*j - 1] != DIRECT_CHAR)
 		{
-			new = ft_put_label_in_block(block, i, str, *j);
-			new = ft_memalloc(sizeof(t_chain));
-			return (new);
+			ft_put_label_in_block(block, i, str, *j);
+			*j = *j + 1;
+			return (0);
 		}
 		*j = *j + 1;
 	}
-	new = ft_put_line_in_block(block, &i, str);
-	new = ft_memalloc(sizeof(t_chain));
+	nb_arg = ft_put_line_in_block(block, &i, str);
 	*j = i;
-	return (new);
+	return (nb_arg);
 }
 
 void	ft_pass_space(char *str, int *j)
@@ -202,17 +186,24 @@ void	ft_get_in_chain(char *str, int j)
 {
 	t_chain		*block;
 	void		*start;
+	int			nb_arg;
+	int			i;
 
+	nb_arg = 0;
 	j++;
 	block = ft_memalloc(sizeof(t_chain));
 	block->next = 0;
-	start = &block;
+	start = block;
+	i = 0;
 	while (str[j])
 	{
 		if (str[j] == '\n')
 			block->nb_lines++;
 		else if (str[j] == '#')
+		{
 			ft_pass_comment(str, &j);
+			continue ;
+		}
 		else if (str[j] == ' ' || str[j] == '\t')
 		{
 			ft_pass_space(str, &j);
@@ -221,8 +212,25 @@ void	ft_get_in_chain(char *str, int j)
 		else if ((!ft_strchr(LABEL_CHARS, str[j])) && str[j] != DIRECT_CHAR)
 			ft_is_an_error(str, j);
 		else
-			block = ft_put_in_block(block, &j, str);
+			nb_arg = ft_put_in_block(block, &j, str);
+		while (nb_arg)
+		{
+			block = block->next;
+			nb_arg--;
+		}
+		block->next = ft_memalloc(sizeof(t_chain));
+		block = block->next;
+		block->content = ft_strdup("\n");
+		block->category = "INSTRUCTION";
+		block->next = ft_memalloc(sizeof(t_chain));
+		block = block->next;
 		j++;
-		ft_putendl("tour");
+	}
+	block = start;
+	while (block->next)
+	{
+		ft_putstr(block->content);
+		ft_putchar(' ');
+		block = block->next;
 	}
 }
--- vm_champs/champs/kola.s
+++ vm_champs/champs/kola.s
@@ -5,7 +5,7 @@ start:
 	st r1, 6
 	live %0
 	ld %10, r2
-	ld %0, r3
+	ld %0,, r3
 	fork %:top
 	bottom:
 	st r3, 104
