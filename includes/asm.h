915877c050c70411e9781bac642c289bcac30de9
Alexandre <34620689+alecott@users.noreply.github.com>
2018-05-22 20:12:36 +0200

    mooving/modif on t_op struct

--- includes/asm.h
+++ includes/asm.h
@@ -29,10 +29,26 @@ typedef struct		s_chain
 	struct s_chain	*next;
 }					t_chain;
 
+typedef struct	s_op
+{
+	char	*str;
+	size_t	nb_args;
+	int		args[3];
+	int		opcode;
+	int		nb_cycles;
+	char	*name;
+	int		ocp;
+	int		label_size;
+}				t_op;
+
 int		main(int argc, char **argv);
 void	ft_pass_comment(char *str, int *i);
 void	ft_parsing(char *str, header_t *header);
 void	ft_is_an_error(char *str, int i);
 t_chain	*ft_get_in_chain(char *str, int j);
 void	ft_verify_label(char *str, int j, t_chain *block);
+t_op	ft_get_op_tab(void);
+t_op	ft_search_op(char *str);
+t_op	ft_get_op(size_t i);
+
 #endif
