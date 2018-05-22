2c4fce9d52fb6e90413383bf854ca4d14752c6c2
Raphael KRIEF <rkrief@e3r2p21.42.fr>
2018-05-22 16:26:14 +0200

    add op in function

--- asm/ft_get_in_chain.c
+++ asm/ft_get_in_chain.c
@@ -6,7 +6,7 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 11:04:44 by rkrief            #+#    #+#             */
-/*   Updated: 2018/05/22 14:11:27 by rkrief           ###   ########.fr       */
+/*   Updated: 2018/05/22 16:24:31 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		ft_take_nb_argument(char *str, int i)
 	int nb_separator;
 
 	nb_separator = 0;
-	while (str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 	{
 		if (str[i] == SEPARATOR_CHAR)
 			nb_separator++;
@@ -51,7 +51,9 @@ void	ft_complete_content(t_chain *block, char *str, int *i)
 		*i = *i + 1;
 	while (str[*i] == ' ' || str[*i] == '\t')
 		*i = *i + 1;
-	if (!ft_strchr(LABEL_CHARS, str[*i]) && str[*i] != DIRECT_CHAR)
+	if (str[*i] == 'r' && ft_isdigit(str[*i + 3]))
+		ft_is_an_error(str, *i);
+	if (!ft_strchr(LABEL_CHARS, str[*i]) && str[*i] != DIRECT_CHAR && str[*i] != '-')
 		ft_is_an_error(str, *i);
 	j = *i;
 	while (str[*i] > 32 && str[*i] != SEPARATOR_CHAR)
@@ -71,23 +73,24 @@ void	ft_complete_content(t_chain *block, char *str, int *i)
 }
 
 
-//int	ft_check_if_instruction(t_chain *block)
-/*{
-  int i;
+int	ft_check_if_instruction(t_chain *block)
+{
+	int i;
 
-  i = 0;
-  while (i < 16)
-  {
-  if (ft_strequ(op_tab[i][0], block->content))
-  {
-  block->nb_op_tab = i;
-  return (1);
-  }
-  i++;
-  }
-  return (0);
-  }
-  */
+	i = 0;
+	(void)block;
+	while (i < 16)
+	{
+		if (ft_strequ(op_tab[i].name, block->content))
+		{
+			block->nb_op_tab = i;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int		ft_put_line_in_block(t_chain *block, int *i, char *str)
 {
 	int	nb_arg;
@@ -100,26 +103,24 @@ int		ft_put_line_in_block(t_chain *block, int *i, char *str)
 	while (str[*i] == ' ' || str[*i] == '\t')
 		*i = *i + 1;
 	j = *i;
-	while (str[*i] >= 'a' && str[*i] <= 'z')
+	while (str[*i] && str[*i] >= 'a' && str[*i] <= 'z')
 		*i = *i + 1;
 	block->content = ft_strnmdup(str, j, *i);
 	block->category = "INSTRUCTION";
-//	if (!(ft_check_if_instruction(block)))
-	//		ft_is_an_error(str, j);
-	nb_arg = ft_take_nb_argument(str, *i);
+	if (!(ft_check_if_instruction(block)))
+		ft_is_an_error(str, *i);
 	while (str[*i] == ' ' || str[*i] == '\t')
-		*i = *i + 1;
-	//	if (nb_arg != op_tab[block->nb_op_tab][1])
-	//	{
-	//		ft_putendl("wrong number of argument");
-	//		ft_is_an_error(str, *i);
-	//	}
-	//	if ((op_tab[block->nb_op_tab][2] == 1) &&
-	//			(op_tab[block->nb_op_tab][3] == T_DIR && str[*i] != DIRECT_CHAR))
-	//		ft_is_an_error(str, *i);
-	//	else if ((block->nb_op_tab == 2 || block->nb_op_tab == 3 || block->nb_op_tab
-	//				== 4 || block->nb_op_tab == 16 || block->nb_op_tab == 10) && ft_is_lib(str, *i))
-	//		ft_is_an_error(str, *i);
+		*i = *i + 1;	
+	nb_arg = ft_take_nb_argument(str, *i);
+	if (nb_arg != op_tab[block->nb_op_tab].nb_param)
+	{
+		ft_putendl("wrong number of argument");
+		ft_is_an_error(str, *i);
+	}
+	else if ((block->nb_op_tab == 2 || block->nb_op_tab == 3 || 
+block->nb_op_tab == 4 || block->nb_op_tab == 16 ||
+block->nb_op_tab == 10) && !ft_is_lib(str, *i))
+		ft_is_an_error(str, *i);
 	block->next = ft_memalloc(sizeof(t_chain));
 	block = block->next;
 	clone = nb_arg;
@@ -154,15 +155,13 @@ int		ft_put_in_block(t_chain *block, int *j, char *str)
 	int i;
 	int nb_arg;
 
-	i = *j;
-	i = i - 1;
+	i = *j - 1;
 	nb_arg = 0;
-	while (str[*j] != '\n')
+	while (str[*j] && str[*j] != '\n')
 	{
 		if (str[*j] == LABEL_CHAR && str[*j - 1] != DIRECT_CHAR)
 		{
 			ft_put_label_in_block(block, i, str, *j);
-//			*j = *j + 1;
 			return (0);
 		}
 		*j = *j + 1;
@@ -177,7 +176,7 @@ void	ft_pass_space(char *str, int *j)
 	int i;
 
 	i = 0;
-	while (str[*j] < 32)
+	while (str[*j] && str[*j] < 32)
 	{
 		if (str[*j] == '\n')
 			return ;
@@ -231,6 +230,7 @@ void	ft_get_in_chain(char *str, int j)
 		}
 		j++;
 	}
+	block->next = NULL;
 	block = start;
 	while (block->next)
 	{
