c5660ecdeb44c4fe9591b4fe7ea6ab3f8cf0a590
Raphael KRIEF <rkrief@e3r2p18.42.fr>
2018-05-24 16:32:42 +0200

    fix the seg when readinf a folder

--- asm/main.c
+++ asm/main.c
@@ -6,7 +6,7 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/14 18:35:51 by rkrief            #+#    #+#             */
-/*   Updated: 2018/05/23 10:18:05 by alecott          ###   ########.fr       */
+/*   Updated: 2018/05/24 16:31:38 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,24 @@ int main(int argc, char **argv)
 	getall = NULL;
 	if (!(fd = open(argv[1], O_RDONLY)))
 		return (0);
+	if (read(fd, 0, 0) < 0)
+	{
+		ft_putendl("Error while trying to read the file");
+		return (0);
+	}
 	while (get_next_line(fd, &str))
 	{
 		if (getall == NULL)
 			getall = ft_strdup(str);
 		else
-			{
-				tmp = getall;
-				getall = ft_strjoin(tmp, "\n");
-				ft_strdel(&tmp);
-				tmp = getall;
-				getall = ft_strjoin(tmp, str);
-				ft_strdel(&tmp);	
-			}
+		{
+			tmp = getall;
+			getall = ft_strjoin(tmp, "\n");
+			ft_strdel(&tmp);
+			tmp = getall;
+			getall = ft_strjoin(tmp, str);
+			ft_strdel(&tmp);	
+		}
 	}
 	ft_parsing(getall, header, argv[1]);
 	return (0);
